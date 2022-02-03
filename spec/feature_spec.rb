d942b6f319afa0514a1233df74ca2d80d437d813
Rakshak Shetty <rakshak_81@hotmail.com>
2022-02-03 15:12:30 +0000

    Unit test 10
    Co-authored-by: AliCo-Hash <AliCo-Hash@users.noreply.github.com>

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -10,11 +10,18 @@ require_relative '../lib/station'
 describe "User Story 9:" do
   it "show previous trips" do
     oyster_card = Oystercard.new
-    entry_station = Station.new("Oxford")
-    exit_station = Station.new("Holborn")
+    entry_station = Station.new("Oxford", 1)
+    exit_station = Station.new("Holborn", 2)
     oyster_card.top_up(5)
     oyster_card.touch_in(entry_station)
     oyster_card.touch_out(exit_station)
     expect(oyster_card.journeys).to include({:entry_station => entry_station, :exit_station => exit_station})
   end
+end
+
+ describe "User Story 10" do
+   it "Shows the Zone" do
+    station = Station.new("Oxford", 1 )
+    expect(station.zone).to eq 1
+   end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,10 +1,18 @@
 require_relative '../lib/station'
 describe Station do
+  subject(:station) {described_class.new("Oxford", 1 )}
+
   describe '# name' do
     it "It should have a Name" do
-      station = Station.new("Oxford")
-
+      
       expect(station.name).to eq("Oxford")
     end
   end
+
+    it "It should have a Zone" do
+  
+
+      expect(station.zone).to eq(1)
+    end
+  
 end
